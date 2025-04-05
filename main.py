b32d9ae396e623b512f485ce12bb45d25be64b66
Saif Rana <saif@ailar.ai>
2025-04-05 16:37:22 +0100

    add check for app settings

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 from fastapi import FastAPI
 import uvicorn
 
-from db.db import init_db, close_db_connection, init_object_storage
+from db.db import init_db, close_db_connection, init_object_storage, get_db
 from routes.routes import setup_routes
 from logger.logger import logger
+from repos.settings_repo import SettingsRepository
 
 # Try to import config - if any required configs are missing, 
 # the app will exit before starting
@@ -25,10 +26,16 @@ setup_routes(app)
 # Startup and shutdown events
 @app.on_event("startup")
 async def startup_db_client():
-     # Validation can be done here too if needed
+    # Validation can be done here too if needed
     logger.info("Starting up application")
     await init_db()
     await init_object_storage()
+    
+    # Initialize settings repository and check settings
+    db = await get_db()
+    settings_repo = SettingsRepository(db)
+    settings = await settings_repo.get_settings()
+    logger.info(f"Current application settings: {settings}")
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
